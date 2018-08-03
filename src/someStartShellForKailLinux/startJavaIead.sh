a223f2367fd5567951ea714e3adff69098cd831b
mengzhigang <mengzhigangnihao@126.com>
2018-08-04 00:11:26 +0800

    change commit

--- src/someStartShellForKailLinux/startJavaIead.sh
+++ src/someStartShellForKailLinux/startJavaIead.sh
@@ -3,9 +3,9 @@
 #description:启动Java集成开发环境idea的脚本
 ########################################################
 #	启动脚本，kial下讲屏幕分辨率设置为1920*1680
-#	参考地址：	http://baijiahao.baidu.com/s?id=1594632413437230067&wfr=spider&for=pc
-#				https://blog.csdn.net/daidaineteasy/article/details/54864776
-#				https://blog.csdn.net/linuxshine/article/details/50717272
+#	参考地址：
+#
+#
 ########################################################
 
 (sudo sh /root/公共/idea-IU-182.3684.101/bin/idea.sh &)
